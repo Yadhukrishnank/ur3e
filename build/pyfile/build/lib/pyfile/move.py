3635c830d3c94883f8dfd67335bf05ae3fe5f27d
yadhukrishnank <yadhunandan97@gmail.com>
2024-12-15 15:31:47 +0000

    trying out git commands

--- build/pyfile/build/lib/pyfile/move.py
+++ build/pyfile/build/lib/pyfile/move.py
@@ -1,48 +1,64 @@
 import rclpy
+from rclpy.action import ActionClient
 from rclpy.node import Node
-from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
-from std_msgs.msg import Header
+from control_msgs.action import FollowJointTrajectory
+from trajectory_msgs.msg import JointTrajectoryPoint
 
-class MoveRobotNode(Node):
+
+class FollowJointTrajectoryClient(Node):
     def __init__(self):
-        super().__init__('move_robot_node')
-        
-        # Publisher for the joint trajectory command
-        self.publisher_ = self.create_publisher(JointTrajectory, '/joint_trajectory_controller/command', 10)
-        
-        # Create a timer to send a message every 1 second
-        self.timer = self.create_timer(1.0, self.timer_callback)
+        super().__init__('follow_joint_trajectory_client')
+        self._action_client = ActionClient(
+            self, FollowJointTrajectory, '/joint_trajectory_controller/follow_joint_trajectory'
+        )
 
-    def timer_callback(self):
-        # Create a JointTrajectory message
-        msg = JointTrajectory()
-        msg.header = Header()
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.joint_names = [
-            'shoulder_pan_joint',
-            'shoulder_lift_joint',
-            'elbow_joint',
-            'wrist_1_joint',
-            'wrist_2_joint',
+    def send_goal(self):
+        goal_msg = FollowJointTrajectory.Goal()
+        
+        # Define joint names
+        goal_msg.trajectory.joint_names = [
+            'shoulder_pan_joint', 
+            'shoulder_lift_joint', 
+            'elbow_joint', 
+            'wrist_1_joint', 
+            'wrist_2_joint', 
             'wrist_3_joint'
         ]
         
-        # Define the target joint positions (in radians)
+        # Define trajectory point
         point = JointTrajectoryPoint()
-        point.positions = [0.5, -0.2, 0.3, -0.5, 0.2, -0.3]  # Example joint positions
-        point.time_from_start.sec = 2  # Move to this position in 2 seconds
-        msg.points.append(point)
+        point.positions = [1.0, 1.0, 1.0, 1.0, 8.0, 1.0]  # Replace with valid positions for your robot
+        point.time_from_start.sec = 6
+        point.time_from_start.nanosec = 0
         
-        # Publish the message
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing joint trajectory command')
+        # Add the trajectory point to the trajectory
+        goal_msg.trajectory.points.append(point)
+        
+        self._action_client.wait_for_server()
+        self.get_logger().info('Sending goal...')
+        self._action_client.send_goal_async(goal_msg).add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected :(')
+            return
+
+        self.get_logger().info('Goal accepted :)')
+        goal_handle.get_result_async().add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info(f'Result received: {result.error_code}')
+        rclpy.shutdown()
+
 
 def main(args=None):
     rclpy.init(args=args)
-    move_robot_node = MoveRobotNode()
-    rclpy.spin(move_robot_node)
-    move_robot_node.destroy_node()
-    rclpy.shutdown()
+    action_client = FollowJointTrajectoryClient()
+    action_client.send_goal()
+    rclpy.spin(action_client)
+
 
 if __name__ == '__main__':
     main()
--- build/ur_description/cmake_install.cmake
+++ build/ur_description/cmake_install.cmake
@@ -43,72 +43,7 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ur_description" TYPE DIRECTORY FILES
-    "/home/yadhu/ur3e/src/ur_description/urdf"
-    "/home/yadhu/ur3e/src/ur_description/config"
-    "/home/yadhu/ur3e/src/ur_description/launch"
-    "/home/yadhu/ur3e/src/ur_description/meshes"
-    "/home/yadhu/ur3e/src/ur_description/rviz"
-    )
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/package_run_dependencies" TYPE FILE FILES "/home/yadhu/ur3e/build/ur_description/ament_cmake_index/share/ament_index/resource_index/package_run_dependencies/ur_description")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/parent_prefix_path" TYPE FILE FILES "/home/yadhu/ur3e/build/ur_description/ament_cmake_index/share/ament_index/resource_index/parent_prefix_path/ur_description")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ur_description/environment" TYPE FILE FILES "/opt/ros/humble/share/ament_cmake_core/cmake/environment_hooks/environment/ament_prefix_path.sh")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ur_description/environment" TYPE FILE FILES "/home/yadhu/ur3e/build/ur_description/ament_cmake_environment_hooks/ament_prefix_path.dsv")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ur_description/environment" TYPE FILE FILES "/opt/ros/humble/share/ament_cmake_core/cmake/environment_hooks/environment/path.sh")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ur_description/environment" TYPE FILE FILES "/home/yadhu/ur3e/build/ur_description/ament_cmake_environment_hooks/path.dsv")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ur_description" TYPE FILE FILES "/home/yadhu/ur3e/build/ur_description/ament_cmake_environment_hooks/local_setup.bash")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ur_description" TYPE FILE FILES "/home/yadhu/ur3e/build/ur_description/ament_cmake_environment_hooks/local_setup.sh")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ur_description" TYPE FILE FILES "/home/yadhu/ur3e/build/ur_description/ament_cmake_environment_hooks/local_setup.zsh")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ur_description" TYPE FILE FILES "/home/yadhu/ur3e/build/ur_description/ament_cmake_environment_hooks/local_setup.dsv")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ur_description" TYPE FILE FILES "/home/yadhu/ur3e/build/ur_description/ament_cmake_environment_hooks/package.dsv")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/packages" TYPE FILE FILES "/home/yadhu/ur3e/build/ur_description/ament_cmake_index/share/ament_index/resource_index/packages/ur_description")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ur_description/cmake" TYPE FILE FILES
-    "/home/yadhu/ur3e/build/ur_description/ament_cmake_core/ur_descriptionConfig.cmake"
-    "/home/yadhu/ur3e/build/ur_description/ament_cmake_core/ur_descriptionConfig-version.cmake"
-    )
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ur_description" TYPE FILE FILES "/home/yadhu/ur3e/src/ur_description/package.xml")
+  include("/home/yadhu/ur3e/build/ur_description/ament_cmake_symlink_install/ament_cmake_symlink_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
