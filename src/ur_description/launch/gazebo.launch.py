86d6d66ca3c87ca54b7334dc78f4d6a3f255a342
Yadhukrishnank <yadhunandan97@gmail.com>
2024-12-20 14:05:10 +0000

    moveit update no:3

--- src/ur_description/launch/gazebo.launch.py
+++ src/ur_description/launch/gazebo.launch.py
@@ -1,32 +1,3 @@
-# Copyright (c) 2021 Stogl Robotics Consulting UG (haftungsbeschränkt)
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#    * Redistributions of source code must retain the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#
-#    * Redistributions in binary form must reproduce the above copyright
-#      notice, this list of conditions and the following disclaimer in the
-#      documentation and/or other materials provided with the distribution.
-#
-#    * Neither the name of the {copyright_holder} nor the names of its
-#      contributors may be used to endorse or promote products derived from
-#      this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Denis Stogl
 
 from launch import LaunchDescription
 from launch.actions import (
@@ -35,7 +6,7 @@ from launch.actions import (
     OpaqueFunction,
     RegisterEventHandler,
 )
-from launch.conditions import IfCondition, UnlessCondition
+from launch.conditions import IfCondition
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
@@ -44,6 +15,7 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def launch_setup(context, *args, **kwargs):
+
     # Initialize Arguments
     ur_type = LaunchConfiguration("ur_type")
     safety_limits = LaunchConfiguration("safety_limits")
@@ -85,7 +57,7 @@ def launch_setup(context, *args, **kwargs):
             safety_k_position,
             " ",
             "name:=",
-            "ur",
+            "ur_gripper",
             " ",
             "ur_type:=",
             ur_type,
@@ -122,7 +94,7 @@ def launch_setup(context, *args, **kwargs):
         executable="spawner",
         arguments=["joint_state_broadcaster",
                    "--controller-manager", "/controller_manager",
-                   "--controller-manager-timeout", "60",
+                   "--controller-manager-timeout", "180",
                    ],
     )
 
@@ -134,12 +106,12 @@ def launch_setup(context, *args, **kwargs):
         )
     )
 
-    joint_controller_spawner = Node(
+    initial_joint_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=[initial_joint_controller,
                    "-c", "/controller_manager",
-                   "--controller-manager-timeout", "60",
+                   "--controller-manager-timeout", "180",
                    ],
     )
 
@@ -158,7 +130,7 @@ def launch_setup(context, *args, **kwargs):
         package="gazebo_ros",
         executable="spawn_entity.py",
         name="spawn_ur",
-        arguments=["-entity", "ur", "-topic", "robot_description", "-timeout", "60"],
+        arguments=["-entity", "ur", "-topic", "robot_description", "-timeout", "180"],
         output="screen",
     )
 
@@ -166,7 +138,7 @@ def launch_setup(context, *args, **kwargs):
         robot_state_publisher_node,
         joint_state_broadcaster_spawner,
         delay_rviz_after_joint_state_broadcaster_spawner,
-        joint_controller_spawner,
+        initial_joint_controller_spawner,
         gazebo,
         gazebo_spawn_robot,
     ]
@@ -182,7 +154,7 @@ def generate_launch_description():
             "ur_type",
             description="Type/series of used UR robot.",
             choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e", "ur20", "ur30"],
-            default_value="ur5e",
+            default_value="ur3e",
         )
     )
     declared_arguments.append(
@@ -218,7 +190,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "controllers_file",
-            default_value="ur_controllers.yaml",
+            default_value="ros2control_controllers.yaml",
             description="YAML file with the controllers configuration.",
         )
     )
@@ -233,7 +205,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "description_file",
-            default_value="ur.urdf.xacro",
+            default_value="ur_gripper.xacro",
             description="URDF/XACRO description file with the robot.",
         )
     )
@@ -254,9 +226,7 @@ def generate_launch_description():
         )
     )
     declared_arguments.append(
-        DeclareLaunchArgument("launch_rviz",
-            default_value="false",
-            description="Launch RViz?")
+        DeclareLaunchArgument("launch_rviz", default_value="false", description="Launch RViz?")
     )
     declared_arguments.append(
         DeclareLaunchArgument(
--- src/ur_moveit_config/launch/ur_moveit.launch.py
+++ src/ur_moveit_config/launch/ur_moveit.launch.py
@@ -273,7 +273,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "description_package",
-            default_value="ur_gripper_gazebo",
+            default_value="ur_description",
             description="Description package with robot URDF/XACRO files. Usually the argument \
         is not set, it enables use of a custom description.",
         )
